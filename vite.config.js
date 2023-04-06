5ee893e86bd99fe991b825e8792edac656fae1d6
maria3213 <fc625@georgetown.edu>
2023-04-06 17:36:32 -0400

    updated

--- vite.config.js
+++ vite.config.js
@@ -16,8 +16,8 @@ export default defineConfig({
       // starts with "/pdfs" request proxy to http://127.0.0.1:3000/pdfs
       // '/pdfs': 'http://127.0.0.1:3000',
       // '/pdf': 'http://127.0.0.1:3000',
-      '/pdfs': 'https://kind-blue-chicken-fez.cyclic.app/',
-      '/pdf': 'https://kind-blue-chicken-fez.cyclic.app/',
+      '/pdfs': 'https://kind-blue-chicken-fez.cyclic.app',
+      '/pdf': 'https://kind-blue-chicken-fez.cyclic.app',
     },
   },
 })
